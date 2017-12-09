a6f8e3e21acbc622178f2682d4b67f0bcdba7a46
laramontana <larabrightside@gmail.com>
2017-12-10 00:35:51 +0300

    Done.

--- src/containers/PetsShow.js
+++ src/containers/PetsShow.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const PetsShow = ({ pet }) => 
+const PetsShow = ({ pet }) =>
   <div className="col-md-8">
     <h2>{pet.name}</h2>
     <p>{pet.description}</p>
   </div>;
 
 const mapStateToProps = (state, ownProps) => {
+  console.log(ownProps)
+  const pet = state.pets.find(pet=>pet.id == ownProps.match.params.petId)
   return {
-    pet: {}
+    pet
   };
 };
 
